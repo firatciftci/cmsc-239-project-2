6b155e43652637840ecdf4d1ec2549e722e82f53
David Yaffe <dayaffe@eevee.cs.uchicago.edu>
2019-06-07 13:59:10 -0500

    some progress with chartjs

--- src/components/example-chart.js
+++ src/components/example-chart.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import {RadialChart, Hint} from 'react-vis';
 
+const SECRETKEY = "$2a$10$95O3pYS8s40pMgpxV.LKI.n5pC9JTNFa3MODMZzfDG2JAZ3mhrIFe";
+
 function groupBy(data, key) {
   return data.reduce((acc, row) => {
     if (!acc[row[key]]) {
@@ -10,6 +12,22 @@ function groupBy(data, key) {
     acc[row[key]].push(row);
     return acc;
   }, {});
+}
+
+export function getElectionsData() {
+  let req = new XMLHttpRequest();
+
+  req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+      console.log(req.responseText);
+    }
+  };
+  
+  req.open("GET", "https://api.jsonbin.io/b/5cfa90ecd2127723845da142", true);
+  req.setRequestHeader("secret-key", SECRETKEY);
+  req.send();
+
+  return req;
 }
 
 export default class ExampleChart extends Component {
